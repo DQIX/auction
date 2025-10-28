791bfd1e34c7961ae784d6f18c424761b9b6b122
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2025-10-28 10:40:50 +0900

    upd

--- _src/mini.bat
+++ _src/mini.bat
@@ -1,3 +1,3 @@
 docker build -t minify .
-docker run --rm -v .:/work minify ./work/data.js ./work/main.js -o ./work/all.min.js -c -m
+docker run --rm -v .:/work minify ./work/main.js -o ./work/all.min.js -c -m
 move "all.min.js" ..\
